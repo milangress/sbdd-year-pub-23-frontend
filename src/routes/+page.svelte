8443c3d2f145e9ca5072e1b3af73cddb4d3ded67
Milan G <dev@milangress.de>
2023-05-07 22:33:11 +0200

    simple WS client

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>WS Client</h1>
+
+{#each messages as message}
+    <p>{message}</p>
+{/each}
+
+<script>
+    import {browser} from '$app/environment';
+
+    let messages = []
+
+
+    if (browser) {
+        const socket = new WebSocket("wss://sbdd-year-pub-23.fly.dev")
+        socket.addEventListener("open", () => {
+            console.log("Opened")
+        })
+        socket.addEventListener("message", (event) => {
+            console.log("Message", event.data)
+            messages = [...messages, event.data]
+        })
+    }
+</script>
