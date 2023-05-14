f364a0d68764440ab297ae7d16f22d61e9b96e02
Milan G <dev@milangress.de>
2023-05-15 01:45:12 +0200

    test

--- src/routes/api/img/+server.js
+++ src/routes/api/img/+server.js
@@ -17,15 +17,16 @@ export async function GET({ url }) {
     })
     console.log(file)
 
-    const asset = await fetch(`https://api.telegram.org/file/${TELEGRAM_BOT_API_KEY}/${file.result.file_path}
-`).then(r => r.blob()).catch(e => {
+    const assetUrl = `https://api.telegram.org/file/bot${TELEGRAM_BOT_API_KEY}/${file.result.file_path}`
+
+    const asset = await fetch(assetUrl).then(r => r.blob()).catch(e => {
         console.log(e)
         return error(404)
     })
     console.log(asset)
     return new Response(asset.stream(), {
-        headers: {
+        /*headers: {
             "Content-Type": asset.type ?? 'image/png',
-        }
+        }*/
     })
 }
